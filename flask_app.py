1429ba6596849f90d27d1ee1c95beb3c113a74c5
cahlering <cahlering@richrelevance.com>
2014-09-02 09:35:45 -0700

    functional proxying to prod

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,25 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, request, Response
+import sys
+sys.path.append("..")
+import proxypy
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='ui', static_url_path='')
 
 @app.route('/')
 def hello_world():
     return 'Hello from Flask!'
 
+@app.route("/rrserver/<path:path>")
+def crossdom(path):
+    proxiedReq = "url=http://recs.richrelevance.com/rrserver/{}?{}".format( path, request.query_string)
+    reply = proxypy.get(proxiedReq)
+    return Response(reply,status=200,mimetype='application/json')
+
+
+
+if __name__ == '__main__':  # pragma: no cover
+    app.debug = True
+    app.run(port=8111)
